487d91f82c41b09ed880a47c65bf1a4d995eb85e
Enrico Ludwig <eludwig.work@gmail.com>
2019-03-27 02:35:35 +0100

    Initial Commit

---
+++ AlbionPNP_Server/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlbionPNP_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
